452098a2566217b54784714b530623150f995760
Elliot Greenlee <elliotgreenlee@gmail.com>
2025-01-18 20:21:08 -0800

    Build an example of the trainer assistant.

--- AI Assistants/trainer_assistant.py
+++ AI Assistants/trainer_assistant.py
@@ -1,6 +1,11 @@
 # Manages exercise planning
 
 from enum import Enum
+import API_Helpers.Google as goog
+import pandas as pd
+from datetime import datetime, timedelta
+from Utils.utils import load_json
+from API_Helpers.openai_utils import OpenAIAPI
 
 
 class WorkoutDay:
@@ -96,108 +101,113 @@ class Yoga(WorkoutSegment):
         super().__init__(sport=Sport.YOGA, duration=duration)
 
 
-def last_week_workout(sheets_service):
+class Training:
+    def __init__(self, df):
+        df = df.drop(index=df.index[0]).reset_index(drop=True)  # Remove example row
+        df['Week Start'] = pd.to_datetime(df['Week Start'], format="%Y/%m/%d")
+        self.df = df
+
+    def this_weeks_workout(self):
+        today = pd.Timestamp(datetime.now())
+        last_week = today - timedelta(days=7)
+
+        this_week = self.df[(self.df['Week Start'] >= last_week) &
+                            (self.df['Week Start'] <= today)]
+
+        pd.set_option('display.max_columns', None)  # Show all columns
+        display_columns = ['Week Start',
+                           'Day 1', 'Day 2', 'Day 3', 'Day 4', 'Day 5', 'Day 6', 'Day 7',
+                           'Total Time']
+
+        if not this_week.empty:
+            return this_week[display_columns]
+        else:
+            print("No exercise plan for this week.")
+
+    def specific_week_workout(self, date):
+        day = pd.to_datetime(date, format="%Y/%m/%d")
+        past_week = day - timedelta(days=7)
+        the_week = self.df[(self.df['Week Start'] >= past_week) &
+                            (self.df['Week Start'] <= day)]
+
+        pd.set_option('display.max_columns', None)  # Show all columns
+        display_columns = ['Week Start',
+                           'Day 1', 'Day 2', 'Day 3', 'Day 4', 'Day 5', 'Day 6', 'Day 7',
+                           'Total Time']
+
+        if not the_week.empty:
+            return the_week[display_columns]
+        else:
+            print("No exercise plan for that week.")
+
+    def workouts_in_dates(self, start_date, end_date):
+        start_day = pd.to_datetime(start_date, format="%Y/%m/%d")
+        end_day = pd.to_datetime(end_date, format="%Y/%m/%d")
+
+        the_weeks = self.df[(self.df['Week Start'] >= start_day) &
+                           (self.df['Week Start'] <= end_day)]
+
+        pd.set_option('display.max_columns', None)  # Show all columns
+        display_columns = ['Week Start',
+                           'Day 1', 'Day 2', 'Day 3', 'Day 4', 'Day 5', 'Day 6', 'Day 7',
+                           'Total Time']
+
+        if not the_weeks.empty:
+            return the_weeks[display_columns]
+        else:
+            print("No exercise plan within those dates.")
+
+
+def workouts_to_prompt(workouts):
+    return workouts.to_string(index=False, justify='left')
+
+
+def example():
+    credentials_path = '../Credentials/api_keys.json'
+    api_keys = load_json(credentials_path)
+
+    scope = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+    user_google_sheets_credentials_path = '../Credentials/google_sheets_user_token.json'
+    google_credentials_path = '../Credentials/google_client.json'
+    google_sheets_api = goog.google_sheets_utils.GoogleSheetsAPI(user_google_sheets_credentials_path, google_credentials_path, scope)
+
     # https://docs.google.com/spreadsheets/d/<spreadsheet_id>/edit
-    training_spreadsheet_id = '1JFpTX7CfaQ-GYvUSiq6dA397tgVxM1v3x_s2vA_UIeQ'
-    training_spreadsheet_range = 'Weekly Training!A1:X25'
-    training_column_types = {
-        'Week': str,
-        'Week Start': str,
-        'Day 1': str,
-        'Day 1 Data': str,
-        'Day 2': str,
-        'Day 2 Data': str,
-        'Day 3': str,
-        'Day 3 Data': str,
-        'Day 4': str,
-        'Day 4 Data': str,
-        'Day 5': str,
-        'Day 5 Data': str,
-        'Day 6': str,
-        'Day 6 Data': str,
-        'Day 7': str,
-        'Day 7 Data': str,
-        'Run Distance': str,
-        'Bike Distance': str,
-        'Swim Distance': str,
-        'Total Distance': str,
-        'Total Time': str,
-        'Running Goal': str,
-        'Biking Goal': str,
-        'Swimming Goal': str
-    }
+    # You will need your own, since it is account based
+    spreadsheet_id = '1JFpTX7CfaQ-GYvUSiq6dA397tgVxM1v3x_s2vA_UIeQ'
+    spreadsheet_range = 'Weekly Training!A1:X25'
+    df = google_sheets_api.load_sheet_to_dataframe(spreadsheet_id, spreadsheet_range)
 
-    # Load data into a Pandas DataFrame
-    df = load_google_sheet_to_dataframe(sheets_service, training_spreadsheet_id,
-                                        training_spreadsheet_range,
-                                        column_types=training_column_types)
+    training = Training(df)
 
-    df['Week Start'] = pd.to_datetime(df['Week Start'], format="%Y/%m/%d")
+    this_week_workouts = training.this_weeks_workout()
+    if not(this_week_workouts is str):
+        this_week_workouts = workouts_to_prompt(this_week_workouts)
 
-    # Get today's date in PST
-    today_pst = pd.Timestamp(datetime.now())
-    start_date = today_pst - timedelta(days=13)
-    end_date = today_pst - timedelta(days=6)
+    today = datetime.now() - timedelta(days=7)
+    six_weeks_ago = today - timedelta(weeks=6)
+    six_weeks_workouts = training.workouts_in_dates(six_weeks_ago.strftime('%Y/%m/%d'), today.strftime('%Y/%m/%d'))
+    if not(six_weeks_workouts is str):
+        six_weeks_workouts = workouts_to_prompt(six_weeks_workouts)
 
-    last_week = df[(df['Week Start'] >= start_date) &
-                   (df['Week Start'] <= end_date)]
+    openai = OpenAIAPI(api_keys['openai_api_key'])
 
-    pd.set_option('display.max_columns', None)  # Show all columns
-    display_columns = ['Week', 'Week Start', 'Day 1', 'Day 2', 'Day 3', 'Day 4', 'Day 5', 'Day 6', 'Day 7']
+    model = "gpt-4o"
+    character = "You are Korra from the tv show Legend of Korra acting as my personal trainer."
+    task = "Give a short paragraph summary of the workouts in the upcoming week. Don't mention rest."
+    workouts = this_week_workouts
+    prompt = character + " " + task + " " + workouts
+    completion = openai.ask(model, prompt)
+    print(completion.choices[0].message.content)
 
-    if not last_week.empty:
-        print(last_week[display_columns])
-    else:
-        print("No exercise plan for last week.")
+    model = "gpt-4o"
+    character = "You are Korra from the tv show Legend of Korra, acting as my personal trainer."
+    goal = "My goal is building cardio and increasing my total time working out to prep for a triathlon training plan."
+    task = "Look at the last few weeks of workouts and suggest four workouts for this week with a terse description."
+    workouts = six_weeks_workouts
+    prompt = character + " " + goal + " " + task + " " + workouts
+    completion = openai.ask(model, prompt)
+    print(completion.choices[0].message.content)
 
 
-def next_week_workout(sheets_service):
-    # https://docs.google.com/spreadsheets/d/<spreadsheet_id>/edit
-    training_spreadsheet_id = '1JFpTX7CfaQ-GYvUSiq6dA397tgVxM1v3x_s2vA_UIeQ'
-    training_spreadsheet_range = 'Weekly Training!A1:X25'
-    training_column_types = {
-        'Week': str,
-        'Week Start': str,
-        'Day 1': str,
-        'Day 1 Data': str,
-        'Day 2': str,
-        'Day 2 Data': str,
-        'Day 3': str,
-        'Day 3 Data': str,
-        'Day 4': str,
-        'Day 4 Data': str,
-        'Day 5': str,
-        'Day 5 Data': str,
-        'Day 6': str,
-        'Day 6 Data': str,
-        'Day 7': str,
-        'Day 7 Data': str,
-        'Run Distance': str,
-        'Bike Distance': str,
-        'Swim Distance': str,
-        'Total Distance': str,
-        'Total Time': str,
-        'Running Goal': str,
-        'Biking Goal': str,
-        'Swimming Goal': str
-    }
-
-    # Load data into a Pandas DataFrame
-    df = load_google_sheet_to_dataframe(sheets_service, training_spreadsheet_id,
-                                        training_spreadsheet_range,
-                                        column_types=training_column_types)
-
-    df['Week Start'] = pd.to_datetime(df['Week Start'], format="%Y/%m/%d")
-
-    # Get today's date in PST
-    today_pst = pd.Timestamp(datetime.now())
-
-    next_week = df[(df['Week Start'] >= today_pst - timedelta(days=6))
-                   & (df['Week Start'] <= today_pst + timedelta(days=1))]
-
-    pd.set_option('display.max_columns', None)  # Show all columns
-    display_columns = ['Week', 'Week Start', 'Day 1', 'Day 2', 'Day 3', 'Day 4', 'Day 5', 'Day 6', 'Day 7']
-    if not next_week.empty:
-        print(next_week[display_columns])
-    else:
-        print("No exercise plan for next week.")
+if __name__ == "__main__":
+    example()
